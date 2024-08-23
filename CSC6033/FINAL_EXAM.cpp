0a1bafdb08f9685540031bc70fa507ef287c91b1
Jacques Plante <jacques.plante@disney.com>
2024-08-22 20:18:05 -0400

    q1 done

--- CSC6033/FINAL_EXAM.cpp
+++ CSC6033/FINAL_EXAM.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int gcd(a, b)
+int gcd(int a, int b)
 {
     if (b == 0)
     {
@@ -15,6 +15,12 @@ int gcd(a, b)
 
 int main()
 {
-    cout >> gcd(10, 34) >> endl;
+    int ex1 = gcd(513, 39);
+    int ex2 = gcd(1812, 210);
+    int ex3 = gcd(1533, 133);
+
+    cout << "the GCD of 513 and 39 is: " << ex1 << endl;
+    cout << "the GCD of 1812 and 210 is: " << ex2 << endl;
+    cout << "the GCD of 1533 and 133 is: " << ex3 << endl;
     return 0;
 }
