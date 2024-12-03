09d67c4835c59021bb76ec8448f2c72369573ee9
Jacques Plante <jacques.plante@disney.com>
2024-12-02 22:34:20 -0500

    Procedure Functions work like a charm

--- CSC6302/Plante-Jacques-final/sql/procedure_functions.sql
+++ CSC6302/Plante-Jacques-final/sql/procedure_functions.sql
@@ -14,11 +14,9 @@ CREATE PROCEDURE AddBoardGame(
     IN p_complexity ENUM('Light', 'Medium Light', 'Medium', 'Medium Heavy', 'Heavy'),
     IN p_categories VARCHAR(255)
 )
+MODIFIES SQL DATA
 BEGIN
     DECLARE v_publisher_id INT;
-    DECLARE v_category_ids VARCHAR(255);
-    DECLARE v_category_id INT;
-    DECLARE v_category_name VARCHAR(50);
     DECLARE v_bg_id INT;
 
     -- Insert or retrieve publisher ID
@@ -26,7 +24,13 @@ BEGIN
     SELECT p_publisher_name
     WHERE NOT EXISTS (SELECT 1 FROM Publishers WHERE name = p_publisher_name)
     LIMIT 1;
-    SET v_publisher_id = LAST_INSERT_ID();
+    
+    IF ROW_COUNT() > 0 THEN
+        SET v_publisher_id = LAST_INSERT_ID();
+    ELSE
+        -- Retrieve existing publisher ID
+        SET v_publisher_id = (SELECT id FROM Publishers WHERE name = p_publisher_name);
+    END IF;
 
     -- Insert board game
     INSERT INTO BoardGames (
@@ -53,45 +57,25 @@ BEGIN
     SET v_bg_id = LAST_INSERT_ID();
 
     -- Insert categories
-    SET v_category_ids = p_categories;
-    WHILE v_category_ids <> '' DO
-        SET v_category_name = SUBSTRING_INDEX(v_category_ids, ',', 1);
-        SET v_category_ids = SUBSTRING(v_category_ids, LENGTH(v_category_name) + 1);
+    SET @category_names = p_categories;
+    WHILE LENGTH(@category_names) > 0 DO
+        SET @category_name = SUBSTRING_INDEX(@category_names, ',', 1);
+        SET @category_names = SUBSTRING(@category_names, LENGTH(@category_name) + 2);
 
         INSERT INTO Categories (name)
-        SELECT v_category_name
-        WHERE NOT EXISTS (SELECT 1 FROM Categories WHERE name = v_category_name)
-        LIMIT 1;
-        SET v_category_id = LAST_INSERT_ID();
+        VALUES (@category_name)
+        ON DUPLICATE KEY UPDATE id = id;
+
+        SET @category_id = (SELECT id FROM Categories WHERE name = @category_name);
 
         INSERT INTO BoardGamesCategories
             (bg_id, cat_id)
-        VALUES (v_bg_id, v_category_id);
+        VALUES
+            (v_bg_id, @category_id);
     END WHILE;
 END//
 
-CREATE PROCEDURE GetBoardGames()
-BEGIN
-    SELECT
-        bg.id, 
-        bg.title, 
-        bg.description, 
-        bg.expansion, 
-        bg.min_players, 
-        bg.max_players, 
-        bg.time_to_play, 
-        bg.min_age, 
-        bg.complexity,
-        p.name AS publisher_name,
-        GROUP_CONCAT(c.name SEPARATOR ', ') AS categories
-    FROM BoardGames bg
-    JOIN Publishers p ON bg.publisher_id = p.id
-    JOIN BoardGamesCategories bgc ON bg.id = bgc.bg_id
-    JOIN Categories c ON bgc.cat_id = c.id
-    GROUP BY bg.id;
-END//
-
-CREATE PROCEDURE GetCategoryGames(category_id INT)
+CREATE PROCEDURE GetBoardGamesWithDetails(have_statement VARCHAR(100))
 READS SQL DATA
 BEGIN
     SET @sql = CONCAT('SELECT
@@ -110,40 +94,29 @@ BEGIN
     JOIN Publishers p ON bg.publisher_id = p.id
     JOIN BoardGamesCategories bgc ON bg.id = bgc.bg_id
     JOIN Categories c ON bgc.cat_id = c.id
-    WHERE bgc.cat_id = ?
-    GROUP BY bg.id;');
-    PREPARE stmt FROM @sql;
-    SET @category_id = category_id;
-    EXECUTE stmt USING @category_id;
-    DEALLOCATE PREPARE stmt;
+    GROUP BY bg.id ');
+    
+    IF have_statement IS NOT NULL THEN
+		SET @sql = CONCAT(@sql, 'HAVING ', have_statement);
+        PREPARE stmt FROM @sql;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+	END IF;
+	PREPARE stmt FROM @sql;
+	EXECUTE stmt;
+	DEALLOCATE PREPARE stmt;
 END//
 
-CREATE PROCEDURE GetPublisherGames(publisher_id INT)
+CREATE PROCEDURE GetCategoryGames(category_name VARCHAR(50))
 READS SQL DATA
 BEGIN
-    SET @sql = CONCAT('SELECT
-        bg.id, 
-        bg.title, 
-        bg.description, 
-        bg.expansion, 
-        bg.min_players, 
-        bg.max_players, 
-        bg.time_to_play, 
-        bg.min_age, 
-        bg.complexity,
-        p.name AS publisher_name,
-        GROUP_CONCAT(c.name SEPARATOR \', \') AS categories
-    FROM BoardGames bg
-    JOIN Publishers p ON bg.publisher_id = p.id
-    JOIN BoardGamesCategories bgc ON bg.id = bgc.bg_id
-    JOIN Categories c ON bgc.cat_id = c.id
-    WHERE p.id = ?
-    GROUP BY bg.id;');
+  CALL GetBoardGamesWithDetails(CONCAT('categories LIKE \'%', category_name, '%\''));
+END//
 
-    PREPARE stmt FROM @sql;
-    SET @publisher_id = publisher_id;
-    EXECUTE stmt USING @publisher_id;
-    DEALLOCATE PREPARE stmt;
+CREATE PROCEDURE GetPublisherGames(publisher_name VARCHAR(50))
+READS SQL DATA
+BEGIN
+    CALL GetBoardGamesWithDetails(CONCAT('publisher_name LIKE \'%', publisher_name, '%\''));
 END//
 
 DELIMITER ;
--- CSC6302/Plante-Jacques-final/sql/query.sql
+++ CSC6302/Plante-Jacques-final/sql/query.sql
@@ -1,18 +1,14 @@
-SELECT
-            bg.id, 
-            bg.title, 
-            bg.description, 
-            bg.expansion, 
-            bg.min_players, 
-            bg.max_players, 
-            bg.time_to_play, 
-            bg.min_age, 
-            bg.complexity,
-            p.name AS publisher_name,
-            GROUP_CONCAT(c.name SEPARATOR ', ') AS categories
-        FROM BoardGames bg
-        JOIN Publishers p ON bg.publisher_id = p.id
-        JOIN BoardGamesCategories bgc ON bg.id = bgc.bg_id
-        JOIN Categories c ON bgc.cat_id = c.id
-        WHERE c.id = 3
-        GROUP BY bg.id;
+CALL AddBoardGame(
+    'Test',
+    'Test Description',
+    'My New Publisher',
+    FALSE,
+    2,
+    3,
+    50,
+    8,
+    1,
+    'Another New Category,Something Else'
+);
+
+SELECT * FROM BoardGames;
