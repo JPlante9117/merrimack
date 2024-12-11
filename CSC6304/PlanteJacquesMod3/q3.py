f8949aae2228dc1387a18c3a2e89c8331bd6cda7
Jacques Robert Plante <plantejac@merrimack.edu>
2024-12-10 20:22:50 -0500

    q3

--- CSC6304/PlanteJacquesMod3/q3.py
+++ CSC6304/PlanteJacquesMod3/q3.py
@@ -1,25 +1,25 @@
 import requests
-import datetime
+from datetime import datetime
 
 def getForecast(url):
     response = requests.get(url)
 
     if response.ok:
         forecast_json = response.json()
-        today = forecast_json.properties.periods[0]
-        todayDate = datetime.fromisoformat(today.startTime)
-        todayDateFormatted = todayDate.strftime('%m/%d/Y')
-        tomorrow = forecast_json.properties.periods[1]
-        tomorrowDate = datetime.fromisoformat(tomorrow.startTime)
-        tomorrowDateFormatted = tomorrowDate.strftime('%m/%d/Y')
-        forecast_string = f'{today.name} ({todayDateFormatted}) the expected temperature is {today.temperature}{today.temperatureUnit} and {tomorrow.name} ({tomorrowDateFormatted}) the expected temperature is {tomorrow.temperature}{tomorrow.temperatureUnit}'
+        today = forecast_json['properties']['periods'][0]
+        todayDate = datetime.fromisoformat(today['startTime'])
+        todayDateFormatted = todayDate.strftime('%m/%d/%Y')
+        tomorrow = forecast_json['properties']['periods'][1]
+        tomorrowDate = datetime.fromisoformat(tomorrow['startTime'])
+        tomorrowDateFormatted = tomorrowDate.strftime('%m/%d/%Y')
+        forecast_string = f'{today['name']} ({todayDateFormatted}) the expected temperature is {today['temperature']}{today['temperatureUnit']} and {tomorrow['name']} ({tomorrowDateFormatted}) the expected temperature is {tomorrow['temperature']}{tomorrow['temperatureUnit']}.'
         
         return forecast_string
     else:
         return 'There was an error fetching the data . . .'
 
 def main():
-    url = 'https://api.weather.gov/gridpoints/OKX/33,35/forecast'
+    url = input('Provide a valid api.weather.gov gridpoint url (Hit enter to just do NYC): ') or 'https://api.weather.gov/gridpoints/OKX/33,35/forecast'
     print(getForecast(url))
 
 main()
